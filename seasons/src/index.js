63e72ebf6ad6d344b3b9be02a4be9c6abda151a0
Camila <camiluc.peli@gmail.com>
2019-07-07 22:52:45 +0200

    Learning about states.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render(){
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
+    
+    render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
