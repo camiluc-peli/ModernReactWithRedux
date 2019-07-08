419455668809f1fa60d10aa6a5f814411bbf2901
Camila <camiluc.peli@gmail.com>
2019-07-08 18:24:49 +0200

    Some refactoring to eliminate warnings.

--- pics/src/components/ImagesList.js
+++ pics/src/components/ImagesList.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const ImageList = (props) => {
     
-    const images = props.images.map((image) => {
-        return <img src={image.urls.regular}/>
+    const images = props.images.map(({ description, id, urls}) => {
+        return <img alt={description} key={id} src={urls.regular}/>
     });
 
 
     return (
-        <diV>
+        <div>
             {images}
-        </diV>
+        </div>
     );
 }
 
