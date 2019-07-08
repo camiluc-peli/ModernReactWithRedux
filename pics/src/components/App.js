0e7bdeb490404baf9736dd568ad203672ed21c54
Camila <camiluc.peli@gmail.com>
2019-07-08 17:09:42 +0200

    Setting the state and getting an error because the context.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,6 +5,8 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component
 {
+    state = { images: [] };
+
     async onSearchSubmit(term){
         const response = await axios.get(
             'https://api.unsplash.com/search/photos', 
@@ -18,13 +20,14 @@ class App extends React.Component
             }
         );
 
-        console.log(response.data.results);
+        this.setState({ images: response.data.results });
     }
 
     render(){
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.setState.images.length} images.
             </div>
         );
     }
